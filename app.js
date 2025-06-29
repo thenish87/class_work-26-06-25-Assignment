80060cd6625a1e0e166ea9cf7a110d50a3fc4ced
Muhammad Danish Ahmed <danish43387@gmail.com>
2025-06-29 19:17:47 +0500

    update 29-06-25

--- app.js
+++ app.js
@@ -63,6 +63,15 @@ console.log(removeDuplicate([1, 1, 2, 3, 2, 4]));
 // 6. Write a function that capitalizes the first letter of each word in a sentence.
 
 
+function capital_letter(str) {
+    str = str.split(" ");
+
+    for (var i = 0, x = str.length; i < x; i++) {
+        str[i] = str[i][0].toUpperCase() + str[i].slice(1).toLowerCase();
+    }
+    return str.join(" ");
+}
+console.log(capital_letter("I am the student of SMIT"));
 
 
 
